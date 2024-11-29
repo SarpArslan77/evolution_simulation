fb4f4c52e8a6b9f6a0d97d45412bc31ab346b07d
SarpArslan77 <157630184+SarpArslan77@users.noreply.github.com>
2024-11-29 10:59:36 +0100

    Update producer_cell.py

--- producer_cell.py
+++ producer_cell.py
@@ -2,7 +2,12 @@ from general import General
 
 import random
 
+#! finish the reproduce logic, copy the predator_cell one and adapt
+
 class Producer_Cell():
+
+    producer_cell_list: list = []
+
     def __init__(self, general: General):
         self.position_x, self.position_y = 0, 0
         self.general = general
@@ -14,10 +19,13 @@ class Producer_Cell():
 
         # 1 = worst, 100 = best
         #   it will be divided by 1000, for example if it is 50 then it is going to be %0.05
-        self.food_production_speed: int = 50
+        self.food_production_speed: int = random.randint(1, 100)
 
         # 1 = worst, 5 = best
-        self.food_production_zone: int = 3
+        self.food_production_zone: int = random.randint(1, 5)
+
+        # 1 = worst, 8 = best
+        self.produce_amount: int = random.randint(1, 8)
 
     def generate_producerCells(self) -> None:
         for _ in range(self.general.starting_generation_producer_cell_count):
@@ -49,20 +57,56 @@ class Producer_Cell():
 
         return (x_position, y_position)
 
-    def produce_food(self) -> None:
-        for cell in self.producer_cell_list:
-            # all the possible coordinates in a 5x5 area
-            directions: list[tuple[int, int]] = [
-                (-2, -2), (-2, -1), (-2, 0), (-2, 1), (-2, 2), 
-                (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2), 
-                (0, -2), (0, -1), (0, 0), (0, 1), (0, 2), 
-                (1, -2), (1, -1), (1, 0), (1, 1), (1, 2), 
-                (2, -2), (2, -1), (2, 0), (2, 1), (2, 2)
-                ]
+    def produce_food(self, producer_cell: "Producer_Cell") -> None:
+        # all the possible coordinates in a 5x5 area
+        directions: list[tuple[int, int]] = [
+            (-2, -2), (-2, -1), (-2, 0), (-2, 1), (-2, 2), 
+            (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2), 
+            (0, -2), (0, -1), (0, 0), (0, 1), (0, 2), 
+            (1, -2), (1, -1), (1, 0), (1, 1), (1, 2), 
+            (2, -2), (2, -1), (2, 0), (2, 1), (2, 2)
+            ]
+        
+        # %0.0(self.food_production_speed)
+        if random.randint(0, 100000) < producer_cell.food_production_speed:
+            dx, dy = random.choice(directions)
+            if producer_cell.general.is_movement_possible(producer_cell.position_x, producer_cell.position_y, dx, dy):
+                producer_cell.general.utility_matrix[producer_cell.position_y+dy][producer_cell.position_x+dx] = "F"
+
+    def reproduce(self, producer_cell: "Producer_Cell") -> None:
+
+        num_children = producer_cell.produce_amount
+
+        for _ in range(num_children):
+
+            new_cell = Producer_Cell(producer_cell.general)
+
+            possible_positions: list[tuple[int, int]] = [
+                (producer_cell.position_x + dx, producer_cell.position_y + dy)
+                for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)]
+            ]
+
+            random.shuffle(possible_positions)
+
+
+    def is_available(self, starting_x: int, starting_y: int, dx: int, dy: int, producer_cell: "Producer_Cell") -> bool:
+
+        # check if the new position is not out of bounds
+        #   is not occupied
+        #   is suitable for producer cells(aka water or plains)
+        if (0 <= starting_x + dx <= producer_cell.general.WORLD_WIDTH // 10 - 1) and (0 <= starting_y + dy <= producer_cell.general.WORLD_HEIGHT // 10 - 1) and \
+            not(producer_cell.general.cell_matrix[starting_y + dy][starting_x + dx]) and \
+            (producer_cell.general.map_matrix[starting_y + dy][starting_x + dx] == 8 or producer_cell.general.map_matrix[starting_y + dy][starting_x + dx] == 9):
             
-            # %0.0(self.food_production_speed)
-            if random.randint(0, 100000) < cell.food_production_speed:
-                dx, dy = random.choice(directions)
-                if self.general.is_movement_possible(cell.position_x, cell.position_y, dx, dy):
-                    self.general.utility_matrix[cell.position_y+dy][cell.position_x+dx] = "F"
-                
+            return True
+        
+        return False
+
+        
+
+
+
+
+
+
+
